7a037a4e50ebbc056e50e5e7f10d9409b212b604
ryanbelmont <ryanbelmont5@gmail.com>
2022-09-30 16:59:05 -0500

    EE2363 lab1 question 1 part b done?

--- EE4363/belmo057_EE4363_lab1/belmo057_EE4363_lab1.srcs/sources_1/imports/EE4363/mux_16to1.v
+++ EE4363/belmo057_EE4363_lab1/belmo057_EE4363_lab1.srcs/sources_1/imports/EE4363/mux_16to1.v
@@ -5,5 +5,15 @@ module mux16to1(D, control, out);
     input [15:0] D;
     input [3:0] control;
     output out;
+    wire w1,w2,w3,w4;
     
+        
+    mux4to1 g1(w1,D[0],D[1],D[2],D[3],control[1],control[0]);
+    mux4to1 g2(w2,D[4],D[5],D[6],D[7],control[1],control[0]);
+    mux4to1 g3(w3,D[8],D[9],D[10],D[11],control[1],control[0]);
+    mux4to1 g4(w4,D[12],D[13],D[14],D[15],control[1],control[0]);
+    
+    mux4to1 g5(out, w1, w2, w3, w4, control[1], control[2]);
+    
+
 endmodule
--- EE4363/belmo057_EE4363_lab1/belmo057_EE4363_lab1.srcs/sources_1/imports/EE4363/mux_4to1.v
+++ EE4363/belmo057_EE4363_lab1/belmo057_EE4363_lab1.srcs/sources_1/imports/EE4363/mux_4to1.v
@@ -1,6 +1,5 @@
 `timescale 1ns / 1ps
 
-
 module mux4to1(out, a, b, c, d, s0, s1);
     input a, b, c, d, s0, s1;
     output out;
