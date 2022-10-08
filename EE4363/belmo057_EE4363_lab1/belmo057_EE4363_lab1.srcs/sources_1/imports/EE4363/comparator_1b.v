023ef9f0f8af9e5b7a3fa11cb6feb4cfacd0fda1
ryanbelmont <ryanbelmont5@gmail.com>
2022-10-07 21:20:44 -0500

    updates

--- EE4363/belmo057_EE4363_lab1/belmo057_EE4363_lab1.srcs/sources_1/imports/EE4363/comparator_1b.v
+++ EE4363/belmo057_EE4363_lab1/belmo057_EE4363_lab1.srcs/sources_1/imports/EE4363/comparator_1b.v
@@ -4,5 +4,9 @@
 module comparator_1b(A, B, less_or_eq);
     input A, B;
     output less_or_eq;
+
+    mux4to1 g1(less_or_eq, 1,1,0,1, B, A);
+    
+    
     
 endmodule
--- EE4363/belmo057_EE4363_lab1/belmo057_EE4363_lab1.srcs/sources_1/imports/EE4363/comparator_4b.v
+++ EE4363/belmo057_EE4363_lab1/belmo057_EE4363_lab1.srcs/sources_1/imports/EE4363/comparator_4b.v
@@ -4,5 +4,11 @@
 module comparator_4b(A, B, less_or_eq);
     input [3:0] A, B;
     output less_or_eq;
+    wire w1, w2, w3;
+    
+    comparator_1b g1(A[3],B[3],w1);
+    comparator_1b g2(A[2],B[2],w2);
+    comparator_1b g3(A[1],B[1],w3);
+    comparator_1b g4(A[0],B[0],less_or_eq);
     
 endmodule
